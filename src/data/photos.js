b6ab37382757723a1b61f4fff61890a3cfb3c721
yizhel <yizhel@alumni.cmu.edu>
2026-05-10 22:40:35 -0400

    Clarified navbar and refreshed favicon

--- src/data/photos.js
+++ src/data/photos.js
@@ -7,14 +7,14 @@
 // 5. caption and location are optional
 
 export const photos = [
-  // {
-  //   id: 'example-1',
-  //   url: 'https://lh3.googleusercontent.com/pw/XXXX=w1600',
-  //   category: 'Travel',       // 'Fencing' | 'Travel' | 'Street' | 'Portrait'
-  //   aspect: 'landscape',      // 'landscape' | 'portrait' | 'square'
-  //   caption: 'Optional caption',
-  //   location: 'Optional location',
-  // },
+  {
+    id: 'photo-1',
+    url: 'https://lh3.googleusercontent.com/pw/AP1GczNt87NAygeeLlJAotJ6FJ4b1UXUWyyJNCQ1zWFVzC-WcQk8jcdsb73Nh1tXjCvEU6km6A9U4AWz9FEAP_NLi6B7ZbrYnnY1JIP-ZifVH1LQCoj0VHr2ONJPPLjibZqOEP3BsCs4mNRODkd24kmfkk1zg',
+    category: 'Nature',
+    aspect: 'landscape',
+    caption: '',
+    location: '',
+  },
 ]
 
-export const CATEGORIES = ['All', 'Fencing', 'Travel', 'Street', 'Portrait']
+export const CATEGORIES = ['All', 'Fencing', 'Travel', 'Street', 'Portrait', 'Nature']
